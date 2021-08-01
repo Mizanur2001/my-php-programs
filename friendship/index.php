73da59fb8805198008b14867a592126a328a48a3
Mizanur <jameslev2001@gmail.com>
2021-08-01 15:21:40 +0530

    Add a new website friendship

---
+++ friendship/index.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Friendship!</title>
+</head>
+
+<body>
+    <?php require "partial/header.php" ?>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://source.unsplash.com/1600x500/?friends" class="d-block w-100"
+                    alt="sorry unable to lode image..">
+            </div>
+            <div class="carousel-item">
+                <img src="https://source.unsplash.com/1600x500/?friends,home" class="d-block w-100"
+                    alt="sorry unable to lode image..">
+            </div>
+            <div class="carousel-item">
+                <img src="https://source.unsplash.com/1600x500/?friendshipday,party" class="d-block w-100"
+                    alt="sorry unable to lode image..">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <h1 class="text-center">Friendship_day</h1>
+    <div class="contaner container-fluid">
+      
+            <div class="row">
+
+                <div class="col-md-4 my-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://source.unsplash.com/500x400/?code,php" alt="Unable to lode image">
+                        <div class="card-body">
+                            <h5 class="card-title">Shramana</h5>
+                            <p class="card-text">Here is some thing inside the card Shramana Please check it out.....</p>
+                            <a href="Shramana.php" class="btn btn-primary">View</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 my-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://source.unsplash.com/500x400/?code,C" alt="Unable to lode image">
+                        <div class="card-body">
+                            <h5 class="card-title">Sruti</h5>
+                            <p class="card-text">Here is some thing inside the card Sruti Please check it out.....</p>
+                            <a href="Sruti.php" class="btn btn-primary">View</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 my-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://source.unsplash.com/500x400/?code,C++" alt="Unable to lode image">
+                        <div class="card-body">
+                            <h5 class="card-title">Nilesh</h5>
+                            <p class="card-text">Here is some thing inside the card Nilesh Please check it out.....</p>
+                            <a href="Nilesh.php" class="btn btn-primary">View</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 my-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://source.unsplash.com/500x400/?code,C++" alt="Unable to lode image">
+                        <div class="card-body">
+                            <h5 class="card-title">Rush</h5>
+                            <p class="card-text">Here is some thing inside the card Rush Please check it out.....</p>
+                            <a href="Rush.php" class="btn btn-primary">View</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 my-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://source.unsplash.com/500x400/?code,C++" alt="Unable to lode image">
+                        <div class="card-body">
+                            <h5 class="card-title">Kushal</h5>
+                            <p class="card-text">Here is some thing inside the card Kushal Please check it out.....</p>
+                            <a href="Kushal.php" class="btn btn-primary">View</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            </div>
+
+            <?php require "partial/footer.php"?>
+       
+
+        <!-- Optional JavaScript; choose one of the two! -->
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+        </script>
+
+        <!-- Option 2: Separate Popper and Bootstrap JS -->
+        <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
