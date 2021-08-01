0f3eb24eaf96ed85503e5a167fc5f1a069dd2055
Mizanur <jameslev2001@gmail.com>
2021-08-01 18:52:14 +0530

    change file php to html

--- friendship/partial/footer.php
+++
@@ -1,5 +0,0 @@
-<div class="contener-fluid bg-dark text-light ">
-<p class="text-center py-3 mb-0"> © copyright Friendship  <?php $date =date("Y");echo"2020-". $date; ?> 
-|| All right reserved <br>Virtual world's Production</p>
-
-</div>
